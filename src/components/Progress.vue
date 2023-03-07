9f88cfcfc4e3f8da9a9d652e31bfd82ca995fabf
isacfg <isaac.lima.105@ufrn.edu.br>
2023-03-07 16:01:44 -0300

    up

---
+++ src/components/Progress.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="progressBar_progressBar_group__3zJF-">
+    <div class="progressBar_progressBar__18WlE">
+      <div
+        class="progressBar_battery__2TZRX progressBarOutline"
+        style="border-color: rgb(55, 53, 47)">
+        <div
+          class="progressBar_percentage__32Zhp progressBarFill"
+          style="width: 23%; background-color: rgb(55, 53, 47)"></div>
+      </div>
+      <p
+        class="progressBar_progressText__1A3fz progressText"
+        style="color: rgb(55, 53, 47)">
+        Month
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.progressBar_progressBar_group__3zJF- {
+  display: -moz-box;
+  display: flex;
+  -moz-box-orient: vertical;
+  -moz-box-direction: normal;
+  flex-direction: column;
+  width: 98%;
+}
+
+.progressBar_progressBar__18WlE {
+  margin-bottom: 4%;
+  width: 100%;
+  margin-left: 1%;
+}
+
+.progressBar_battery__2TZRX {
+  height: 13.2vh;
+}
+
+.progressBar_battery__2TZRX {
+  width: 75%;
+  border: 3px solid #000;
+  border-radius: 8px;
+  padding: 4px;
+  margin-right: 8px;
+}
+
+.progressBar_percentage__32Zhp {
+  background-color: #000;
+  border-radius: 4px 0 0 4px;
+  height: 100%;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <h1>This is a home page</h1>
+    <Progress />
+  </div>
+</template>
+
+<script>
+import Progress from '../components/Progress.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    Progress,
+  },
+  data() {
+    return {}
+  },
+}
+</script>
+
+<style scoped></style>
